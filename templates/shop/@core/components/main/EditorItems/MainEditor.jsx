0b0364bac28b51f7a315b8795fdeedb0079af0ae
Amir <haghdoostamir@gmail.com>
2023-02-21 12:35:37 +0330

    event page editted

--- templates/shop/@core/components/main/EditorItems/MainEditor.jsx
+++ templates/shop/@core/components/main/EditorItems/MainEditor.jsx
@@ -7,29 +7,23 @@ import {
   EditorList,
   EditorGalery,
 } from "./index.js";
-import EditorHeader from "./Header";
-import { Fragment } from "react";
 
 const MainEditor = (props) => {
-    console.log(props);
-  const data = props.details.map((data) => (
-    <div key={data.id}>
-      {data.type === 1 && <EditorTitle data={data} />}
-      {data.type === 2 && <EditorParagraph data={data} />}
-      {data.type === 3 && <EditorImage data={data} />}
-      {data.type === 4 && <EditorLink data={data} />}
-      {data.type === 5 && <EditorList data={data} />}
-      {data.type === 6 && <EditorMultiMedia data={data} />}
-      {data.type === 7 && <EditorGalery data={data} />}
-    </div>
-  ));
-
-  return(
-    <Fragment>
-        <EditorHeader data={props} />
-        {data}
-    </Fragment>
-  )
+  return (
+    <>
+      {props.details.map((data) => (
+        <div key={data.id}>
+          {data.type === 1 && <EditorTitle data={data} />}
+          {data.type === 2 && <EditorParagraph data={data} />}
+          {data.type === 3 && <EditorImage data={data} />}
+          {data.type === 4 && <EditorLink data={data} />}
+          {data.type === 5 && <EditorList data={data} />}
+          {data.type === 6 && <EditorMultiMedia data={data} />}
+          {data.type === 7 && <EditorGalery data={data} />}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default MainEditor;
--- templates/shop/pages/shopEvent.jsx
+++ templates/shop/pages/shopEvent.jsx
@@ -2,15 +2,49 @@ import { useSelector } from "react-redux";
 import Comments from "../@core/components/main/EditorItems/Comments";
 import Event from "../@core/components/main/Event";
 import "tailwindcss/tailwind.css";
+import { Calendar } from "iconsax-react";
+import Image from "next/image";
+import moment from "jalali-moment";
 import MainEditor from "../@core/components/main/EditorItems/MainEditor";
+import { digitsEnToFa } from "@persian-tools/persian-tools";
 
 const ShopEvent = (props) => {
   const { events } = useSelector((state) => state.businessSlice);
+  console.log(props);
+  const startAt = digitsEnToFa(
+    moment(props.timing.start_at, "YYYY-MM-DD")
+      .locale("fa")
+      .format("YYYY/MM/DD")
+  );
+  const endAt = digitsEnToFa(
+    moment(props.timing.end_at, "YYYY/MM/DD").locale("fa").format("YYYY/MM/DD")
+  );
 
   return (
     <div className="container mt-16 px-4">
       <div className="container max-w-[900px] flex flex-col gap-8">
-        <MainEditor {...props}/>
+        <div className="flex items-center justify-between">
+          <h3 className="font-bold text-3xl">{props.title}</h3>
+          <div className="text-sm flex items-center justify-center">
+            <Calendar className="text-skin-primary ml-3" />
+            <p className="mt-1">
+              <span className="text-skin-primary">از</span> {startAt}{" "}
+              <span className="mx-3 text-[skin-color]">
+                {" "}
+                <span className="text-skin-primary">تا</span>
+              </span>{" "}
+              {endAt}
+            </p>
+          </div>
+        </div>
+        <Image
+          width={500}
+          height={50}
+          src={props.files[0].details.location}
+          alt={props.title}
+          className="rounded-xl w-full"
+        />
+        <MainEditor {...props} />
         <Comments />
         <section className="md:flex">
           {events?.slice(0, 2)?.map((item) => (
